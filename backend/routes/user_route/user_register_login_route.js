887a557d8b1d2add554f217d2a9af0331aa9352b
abhijit732023 <abhijitgautam2004@gmail.com>
2025-05-02 00:20:20 +0530

    one

--- backend/routes/user_route/user_register_login_route.js
+++ backend/routes/user_route/user_register_login_route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import cookieParser from "cookie-parser"; 
 import jwt from "jsonwebtoken"; 
 import{user_register_model,config} from '../../index.js'
